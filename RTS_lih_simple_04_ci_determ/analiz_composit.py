cc07d7f83b7c6928cc22ffc7dc41b60d4202c7fc
Aleksey Manin <Alkor135@gmail.com>
2025-04-02 22:25:13 +0300

    update analiz_composit.py

--- RTS_lih_simple_04_ci_determ/analiz_composit.py
+++ RTS_lih_simple_04_ci_determ/analiz_composit.py
@@ -74,17 +74,10 @@ def recovery_factor(cagr_value, mdd):
 
 # Коэффициент Шарпа: Средняя доходность / Стандартное отклонение * sqrt(252)
 def sharpe_ratio(daily_returns, risk_free_rate=0.0, periods_per_year=252):
-    # # Убираем NaN значения из доходностей
-    # daily_returns = daily_returns.dropna()
-
-    # # Если после удаления NaN остались данные, считаем
-    # if daily_returns.std() > 0:
-    #     excess_returns = daily_returns - risk_free_rate
-    #     return excess_returns.mean() / excess_returns.std() * np.sqrt(periods_per_year)
-    # else:
-    #     return np.nan
-
+    # Убираем NaN значения из доходностей
     daily_returns = daily_returns.replace([np.inf, -np.inf], np.nan).dropna()
+
+    # Если после удаления NaN остались данные, считаем
     if daily_returns.std() > 0:
         excess_returns = daily_returns - risk_free_rate
         return excess_returns.mean() / excess_returns.std() * np.sqrt(periods_per_year)
@@ -94,19 +87,10 @@ def sharpe_ratio(daily_returns, risk_free_rate=0.0, periods_per_year=252):
 
 # Коэффициент Сортино: Средняя доходность / Downside Std * sqrt(252)
 def sortino_ratio(daily_returns, risk_free_rate=0.0, periods_per_year=252):
-    # # Убираем NaN значения из доходностей
-    # daily_returns = daily_returns.dropna()
-
-    # # Если после удаления NaN остались данные, считаем
-    # if len(daily_returns[daily_returns < 0]) > 0:
-    #     excess_returns = daily_returns - risk_free_rate
-    #     downside_std = excess_returns[excess_returns < 0].std()
-    #     return excess_returns.mean() / downside_std * np.sqrt(
-    #         periods_per_year) if downside_std > 0 else np.nan
-    # else:
-    #     return np.nan
-
+    # Убираем NaN значения из доходностей
     daily_returns = daily_returns.replace([np.inf, -np.inf], np.nan).dropna()
+
+    # Если после удаления NaN остались данные, считаем
     if len(daily_returns[daily_returns < 0]) > 0:
         excess_returns = daily_returns - risk_free_rate
         downside_std = excess_returns[excess_returns < 0].std()
@@ -118,24 +102,24 @@ def sortino_ratio(daily_returns, risk_free_rate=0.0, periods_per_year=252):
 # Загрузка данных. 'TRADEDATE' в индекс.
 df = pd.read_csv("pred_res_cum.csv", index_col=0, parse_dates=True)
 
-df = df.loc[:, df.columns.str.startswith('CUM')]
+df_cum = df.loc[:, df.columns.str.startswith('CUM')]
 
 # Заменяем значения в строке с индексом 0 на 0 для колонок, начинающихся с 'CUM'
 # Получаем дату из индекса нулевой строки
-first_date = df.index[0]  # Индекс первой строки
+first_date = df_cum.index[0]  # Индекс первой строки
 # Проверяем, существует ли строка с этой датой
-if first_date in df.index:
+if first_date in df_cum.index:
     # Записываем нули в колонки, начинающиеся с 'CUM'
-    df.loc[first_date, df.filter(like='CUM').columns] = 0
+    df_cum.loc[first_date, df_cum.filter(like='CUM').columns] = 0
 else:
     print(f"Дата {first_date} отсутствует в индексе DataFrame.")
-# print(df)
+# print(df_cum)
 
 # Расчет метрик для каждой модели
 results = []
 
-for column in df.columns:
-    cum_returns = df[column].dropna()
+for column in df_cum.columns:
+    cum_returns = df_cum[column].dropna()
 
     if cum_returns.empty or cum_returns.nunique() == 1:
         print(f"Warning: {column} has insufficient data")
@@ -180,6 +164,28 @@ for column in df.columns:
 df_results = pd.DataFrame(results).sort_values(by="Smoothness R²", ascending=False)
 
 df_results = df_results.round(4).fillna("-")
+df_results = df_results.head(10)
 
 # print(df_results.to_string(index=False))
-print(df_results.head(10).to_string(max_rows=10, max_cols=20))
+# print(df_results.head(10).to_string(max_rows=10, max_cols=20))
+print(df_results)
+
+# Извлекаем числа из колонки 'Model' и преобразуем их в список
+numbers = df_results["Model"].str.extract(r'(\d+)')[0].astype(int).tolist()
+
+print(numbers)
+
+# Проверяем наличие колонок перед выборкой
+required_columns = [
+    'DIRECTION', f'PRED_{numbers[0]}', f'RES_{numbers[0]}', f'PRED_{numbers[1]}', f'RES_{numbers[1]}'
+]
+missing_columns = [col for col in required_columns if col not in df.columns]
+
+df_tmp = pd.DataFrame()  # Инициализируем пустой DataFrame
+if missing_columns:
+    print(f"Отсутствующие колонки: {missing_columns}")
+else:
+    df_tmp = df[required_columns].copy()
+    # print(df_tmp)
+
+print(df_tmp)
